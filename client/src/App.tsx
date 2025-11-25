7b500ac7110590cbdf930ba76c2fdfa8662142df
pheldu95 <phelps.duncan@gmail.com>
2025-11-25 11:51:25 -0600

    created DeleteButton functional componenet

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from 'react'
 import './App.css'
 import type { FishSpecies } from './lib/types';
 import NavBar from './components/NavBar';
+import DeleteButton from './components/buttons/DeleteButton';
 
 function App() {
   const [fishSpecies, setFishSpecies] = useState<FishSpecies[]>([]);
@@ -19,7 +20,12 @@ function App() {
       <h1 className="text-3xl font-bold text-blue-600 bg-yellow-200 p-4 rounded-lg">Boundary Waters Fish Map</h1>
       <ul>
         {fishSpecies.map((species) => (
-          <li key={species.id}>{species.name}</li>
+          <li key={species.id}>
+            {species.name}
+            <div className='max-w-small'>
+              <DeleteButton/>
+            </div>
+          </li>
         ))}
       </ul>
     </>
---
+++ client/src/components/buttons/DeleteButton.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function DeleteButton() {
+    return (
+        <button
+            className="w-full text-gray-900 bg-red-500 hover:bg-red-600 focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 max-w-sm text-center cursor-pointer"
+        >
+            DeleteButton
+        </button>
+    )
+}
