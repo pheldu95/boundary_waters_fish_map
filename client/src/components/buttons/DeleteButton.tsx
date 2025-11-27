89b6456138d2bbf36937c72366ce1ae7ed9011b7
pheldu95 <phelps.duncan@gmail.com>
2025-11-26 21:24:31 -0600

    centered the content of the delete button

--- client/src/components/buttons/DeleteButton.tsx
+++ client/src/components/buttons/DeleteButton.tsx
@@ -10,10 +10,10 @@ export default function DeleteButton<TArgs, TError = Error>({
     deleteMutation,
 }: DeleteButtonProps<TArgs, TError>) {
     const isDeleting = deleteMutation.isPending && deleteMutation.variables === itemId;
-    
+
     return (
         <button
-            className="w-full text-gray-900 bg-red-500 hover:bg-red-600 focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 max-w-sm text-center cursor-pointer disabled:opacity-50"
+            className="w-full text-gray-900 bg-red-500 hover:bg-red-600 focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 max-w-sm cursor-pointer disabled:opacity-50 flex items-center justify-center"
             disabled={isDeleting}
             onClick={() => deleteMutation.mutate(itemId)}
         >
