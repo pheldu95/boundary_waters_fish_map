850ca8f786e2473ae277ff63b5759838ab6f8da2
pheldu95 <phelps.duncan@gmail.com>
2025-12-02 11:17:09 -0600

    got rid of default button hover text color change

--- client/src/components/buttons/DefaultButton.tsx
+++ client/src/components/buttons/DefaultButton.tsx
@@ -7,7 +7,7 @@ export default function DefaultButton({ text, onClickProps }: DefaultButtonProps
     return (
         <button
             className="group px-8 py-4 bg-foresty text-yellowishbone font-mono font-bold 
-         hover:bg-forestyhover hover:text-fishblue transition-colors 
+         hover:bg-forestyhover transition-colors 
          shadow-[4px_4px_0px_0px_rgba(0,0,0,0.1)]
          hover:shadow-none hover:translate-x-[2px] hover:translate-y-[2px] 
          transition-all cursor-pointer"
