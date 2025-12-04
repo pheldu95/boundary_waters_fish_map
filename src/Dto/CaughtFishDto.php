1d3b362a84c3a6faac30fc785340e9cfbeb71259
pheldu95 <phelps.duncan@gmail.com>
2025-12-03 22:41:07 -0600

    created a DTO for CaughtFish. Will be used when submitting form data

---
+++ src/Dto/CaughtFishDto.php
@@ -0,0 +1,44 @@
+<?php
+namespace App\Dto;
+
+use Symfony\Component\Validator\Constraints as Assert;
+
+final class CaughtFishDto
+{
+    #[Assert\NotBlank]
+    public string $latitude;
+
+    #[Assert\NotBlank]
+    public string $longitude;
+
+    #[Assert\NotNull]
+    public \DateTimeImmutable $caughtDate;
+
+    #[Assert\Positive]
+    public ?float $length;
+
+    #[Assert\NotBlank]
+    public int $fishSpeciesId;
+
+    public ?int $fishingLureId;
+
+    public ?string $note = null;
+
+    public function __construct(
+        string $latitude,
+        string $longitude,
+        \DateTimeImmutable $caughtDate,
+        ?float $length,
+        int $fishSpeciesId,
+        ?int $fishingLureId,
+        ?string $note = null
+    ) {
+        $this->latitude = $latitude;
+        $this->longitude = $longitude;
+        $this->caughtDate = $caughtDate;
+        $this->length = $length;
+        $this->fishSpeciesId = $fishSpeciesId;
+        $this->fishingLureId = $fishingLureId;
+        $this->note = $note;
+    }
+}
