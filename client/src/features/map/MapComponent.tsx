31c3a6700da4089dd65be947cbf55ee19ab08a3a
pheldu95 <phelps.duncan@gmail.com>
2025-12-11 14:22:15 -0600

    made maxZoom on map higher

--- client/src/features/map/MapComponent.tsx
+++ client/src/features/map/MapComponent.tsx
@@ -29,7 +29,7 @@ export default function MapComponent({ addingCaughtFish, filters }: Props) {
                 attribution='Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a>'
                 url="https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png"
                 maxNativeZoom={17} // This tile layer goes up to zoom level 16
-                maxZoom={20} // Allow zooming in further. Makes map blurry though
+                maxZoom={30} // Allow zooming in further. Makes map blurry though
             />
             <MarkerClusterGroup
                 chunkedLoading
